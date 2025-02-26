2b6fd06422c05af226ca6950e6ba8c6cb8543b49
Abhijeet.maity <Abhijeet.maity@nimapinfotech.in>
2025-02-26 17:52:37 +0530

    Changes done

--- src/components/ValidatedWithYupForm.jsx
+++ src/components/ValidatedWithYupForm.jsx
@@ -13,29 +13,7 @@ const onSubmit = (values) => {
   console.log("Form Data", values);
 };
 
-const validate = (values) => {
-  //values.name values.email values.channel
-  //errors.name errors.email errors.channel
-  //errors.name = "This field is required"
 
-  let errors = {};
-  if (!values.name) {
-    errors.name = "Required";
-  }
-
-  if (!values.email) {
-    errors.email = "Required";
-  } else if (
-    !/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(values.email)
-  ) {
-    errors.email = "Invalid email format";
-  }
-
-  if (!values.channel) {
-    errors.channel = "Required";
-  }
-  return errors;
-};
 
 const validationSchema = Yup.object({
   name: Yup.string().required("Required!!"),
