a304a44321507030c62520e53f24fc86f0eb366e
Abhijeet.maity <Abhijeet.maity@nimapinfotech.in>
2025-02-26 17:52:01 +0530

    Added Formik Components

--- src/components/FormikComponentForm.jsx
+++ src/components/FormikComponentForm.jsx
@@ -1,108 +1,132 @@
 import React from "react";
-import { useFormik } from "formik";
+import { Formik, Form, Field, ErrorMessage, FieldArray } from "formik";
 import * as Yup from "yup";
 import "../App.css";
+import TextError from "./TextError";
 
 const initialValues = {
   name: "",
   email: "",
   channel: "",
+  comments: "",
+  address: "",
+  social: {
+    facebook: "",
+    twitter: "",
+  },
+  phoneNumbers: ["",""],
+  skills: [""],
 };
 
 const onSubmit = (values) => {
   console.log("Form Data", values);
 };
 
-const validate = (values) => {
-  //values.name values.email values.channel
-  //errors.name errors.email errors.channel
-  //errors.name = "This field is required"
-
-  let errors = {};
-  if (!values.name) {
-    errors.name = "Required";
-  }
-
-  if (!values.email) {
-    errors.email = "Required";
-  } else if (
-    !/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(values.email)
-  ) {
-    errors.email = "Invalid email format";
-  }
-
-  if (!values.channel) {
-    errors.channel = "Required";
-  }
-  return errors;
-};
-
 const validationSchema = Yup.object({
   name: Yup.string().required("Required!!"),
   email: Yup.string().email("Invalid email format").required("Required!!"),
   channel: Yup.string().required("Required!!"),
+  address: Yup.string().required("Required"),
 });
 
 const FormikComponentForm = () => {
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-    validationSchema,
-  });
-
-  console.log("formik values", formik.values); //returns values of all the fields.
-  console.log("formik errors", formik.errors); //returns error object which contains an info about the field containing error
-  console.log("Visited fields", formik.touched); //returns an object containing names of input field which are visited.
-
   return (
-    <div className="form Formik-component">
-      <h1>Formik Component Library</h1>
-      <form action="" onSubmit={formik.handleSubmit}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      <Form className="form Formik-component">
         <div className="form-controls">
           <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            {...formik.getFieldProps("name")}
-            // onChange={formik.handleChange}
-            // onBlur={formik.handleBlur}
-            // value={formik.values.name}
-          />
-          {formik.touched.name && formik.errors.name ? (
-            <div className="error">{formik.errors.name}</div>
-          ) : null}
+          <Field type="text" id="name" name="name" />
+          <ErrorMessage name="name" component={TextError} />
         </div>
 
         <div className="form-controls">
           <label htmlFor="email">E-mail</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            {...formik.getFieldProps("email")}
-          />
-          {formik.touched.email && formik.errors.email ? (
-            <div className="error">{formik.errors.email}</div>
-          ) : null}
+          <Field type="email" id="email" name="email" />
+          <ErrorMessage name="email" component={TextError} />
         </div>
 
         <div className="form-controls">
           <label htmlFor="channel">Channel</label>
-          <input
-            type="text"
-            id="channel"
-            name="channel"
-            {...formik.getFieldProps("channel")}
-          />
-          {formik.touched.channel && formik.errors.channel ? (
-            <div className="error">{formik.errors.channel}</div>
-          ) : null}
+          <Field type="text" id="channel" name="channel" />
+          <ErrorMessage name="channel" component={TextError} />
+        </div>
+
+        <div className="form-controls">
+          <label htmlFor="comments">Comments</label>
+          <Field as="textarea" id="comments" name="comments" />
+          <ErrorMessage name="comments" component={TextError} />
+        </div>
+
+        <div className="form-controls">
+          <label htmlFor="address">Address</label>
+          <Field name="address">
+            {(props) => {
+              const { field, form, meta } = props;
+              console.log("Render props", props, form);
+              return (
+                <>
+                  <input
+                    type="text"
+                    id="address"
+                    placeholder="Enter address"
+                    {...field}
+                    onBlur={field.onBlur}
+                  />
+                  {meta.touched && meta.error ? (
+                    <div className="error">{meta.error}</div>
+                  ) : null}
+                </>
+              );
+            }}
+          </Field>
+        </div>
+
+        <div className="form-controls">
+          <label htmlFor="facebook">Facebook profile</label>
+          <Field type="text" id="facebook" name="social.facebook" />
+        </div>
+
+        <div className="form-controls">
+          <label htmlFor="facebook">Twitter profile</label>
+          <Field type="text" id="twitter" name="social.twitter" />
+        </div>
+
+        <div className="form-controls">
+          <label htmlFor="PrimaryPhone">Primary Phone Number</label>
+          <Field type="text" id="PrimaryPhone" name="phoneNumbers[0]" />
+        </div>
+
+        <div>
+          <label htmlFor="SecondaryPhone">Secondary Phone Number</label>
+          <Field type="text" id="SecondaryPhone" name="phoneNumbers[1]" />
+        </div>
+
+        <div className="form-controls">
+          <label htmlFor="">List of Skills</label>
+          <FieldArray name="skils">
+            {
+              (FieldArrayProps) => {
+                console.log("FieldArrayProps", FieldArrayProps);
+                const {push, remove, form} = FieldArrayProps;
+                const {values} = form;
+                const {skills} = values;
+                return (
+                  <div>
+                    FieldArrayProps
+                  </div>
+                )
+              }
+            }
+          </FieldArray>
         </div>
 
         <button type="submit">Submit</button>
-      </form>
-    </div>
+      </Form>
+    </Formik>
   );
 };
 
