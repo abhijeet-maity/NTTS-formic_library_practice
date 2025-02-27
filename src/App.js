d3660b8406dea750e79336c8caceab26f5fd5d6d
Abhijeet.maity <Abhijeet.maity@nimapinfotech.in>
2025-02-27 17:49:18 +0530

    added one new component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import SampleForm from './components/SampleForm';
 import ValidateWithYupForm from './components/ValidatedWithYupForm';
 import FormikComponentForm from './components/FormikComponentForm';
 import './App.css';
+import FormikComponentRenderProps from './components/FormikComponentRenderProps';
 
 
 function App() {
+
   return (
     <div className="App">
       
-      <SampleForm/>
+      {/* <SampleForm/>
       
-      <ValidateWithYupForm/>
+      <ValidateWithYupForm/> */}
     
-      <FormikComponentForm/>
+      {/* <FormikComponentForm/> */}
+
+      <FormikComponentRenderProps/>
     </div>
   );
 }
